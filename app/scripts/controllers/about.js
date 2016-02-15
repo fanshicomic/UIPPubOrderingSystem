49654b2820292e5e7dfc74da7d6ef5fdc066f346
fanshicomic <fanshicomic@gmail.com>
2016-02-15 14:17:59 +0100

    re-initilize project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pubOsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pubOsApp
+ */
+angular.module('pubOsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pubOsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pubOsApp
+ */
+angular.module('pubOsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
